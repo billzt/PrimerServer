45c27a4e22eb35fcb94674b2394e57c5aa00d565
Zhu Tao <bill_zt@sina.com>
2017-03-16 09:08:40 +0800

    Rename modal.php to modal_MFEPrimer_result.php

--- script/modal.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-session_start();
-$session_id = session_id();
-$date = date("Y-m-d");
-$working_dir = "/tmp/Primer-$date-$session_id";
-
-$file = $_GET['file'];
-echo file_get_contents("$working_dir/tmp.MFEPrimer/$file");
